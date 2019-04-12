a7adcd8cdaf9b95a5c8de03b2565d144f4a65259
549595297@qq.com <jia327926>
2019-04-12 16:02:55 +0800

    修改

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const { spawnSync } = require('child_process');
 const findChrome = require('chrome-finder');
@@ -6,6 +7,19 @@ const DefinePlugin = require('webpack/lib/DefinePlugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const EndWebpackPlugin = require('end-webpack-plugin');
 const { WebPlugin } = require('web-webpack-plugin');
+const ghpages = require('gh-pages');
+
+function publishGhPages() {
+  return new Promise((resolve, reject) => {
+    ghpages.publish(outputPath, { dotfiles: true }, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    })
+  });
+}
 
 const outputPath = path.resolve(__dirname, '.public');
 module.exports = {
@@ -81,12 +95,19 @@ module.exports = {
       allChunks: true,
     }),
     new EndWebpackPlugin(async () => {
+      // 自定义域名
+      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'jl.githink.cn');
+
+      //await publishGhPages();
+
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, '毛子坤的简历.pdf')}`,
-        'http://resume.githink.cn' // 这里注意改成你的在线简历的网站
+        'https://jl.githink.cn/' // 这里注意改成你的在线简历的网站
       ]);
 
+      // // 重新发布到 ghpages
+      // await publishGhPages();
     }),
   ]
 };
